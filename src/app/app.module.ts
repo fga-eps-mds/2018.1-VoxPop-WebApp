174ee6d67a4530032cc27049a07dd0cb68598ed3
Rossicler Rodrigues Pires Júnior <hellslimm@hotmail.com>
2018-04-05 15:01:27 -0300

    Added registerForm and main-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,30 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { MainPageComponent } from './main-page/main-page.component'; 
+import { RouterModule, Routes, Router } from '@angular/router';
 
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: RegisterFormComponent 
+  },
+  {
+    path: 'main-page',
+    component: MainPageComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    MainPageComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/register-form/register-form.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.css']
+})
+export class RegisterFormComponent implements OnInit {
+
+  values = '';
+  password = '';
+  statusPassword = 0; // 0 -> False and 1 -> True
+  registerBtn = document.getElementById("registerBtn");
+  
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  registerUser(e) {
+    e.preventDefault();
+    console.log(e);
+    var username = e.target.elements[0].value;
+    var password = e.target.elements[1].value;
+    var confPassword = e.target.elements[2].value;
+    var email = e.target.elements[3].value;
+    console.log(username, password, email);
+
+    if(this.statusPassword == 1){
+      this.router.navigate(['main-page']); 
+    }
+    else {
+      alert("Check all the boxes, please");
+    }
+    
+  }
+
+  onKeyPass(e: any) {
+    this.password = e.target.value;
+  }
+
+  onKey(e: any) {
+    var confirmPassword = e.target.value;
+
+    if(this.validator(confirmPassword, this.password)){
+      document.getElementById('alert').style.display = "none";
+      this.values = '';
+      this.statusPassword = 1;
+    }
+    else {
+      this.values = 'Passwords do not match!';
+      document.getElementById('alert').style.display = "block";
+      this.statusPassword = 0;
+    }
+  }
+
+  validator(password, confPassword) {
+      if(password === confPassword){
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+  }
+
+}
