7a571db336262889b7dc591f24dd05d32946d50a
Rossicler Rodrigues Pires Júnior <hellslimm@hotmail.com>
2018-04-11 15:50:49 -0300

    Fixed username validator and added email validator

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -14,9 +14,13 @@ export class RegisterFormComponent implements OnInit {
 
   valuePassword = '';
   valueUsername = '';
+  valueEmail = '';
   password = '';
+  confirmPassword = '';
   username = '';
-  status = false;
+  statusPassword = false;
+  statusUsername = false;
+  statusEmail = false;
 
   constructor(private router:Router,
               private requester:RequestsService) { }
@@ -59,33 +63,54 @@ export class RegisterFormComponent implements OnInit {
     this.password = e.target.value;
   }
 
+  onKeyConfirmPassword(e: any) {
+    this.confirmPassword = e.target.value;
+  }
+
   onKeyUsername(e: any) {
     var username = e.target.value;
     if(this.isUsernameValid(username)){
       document.getElementById('alert-username').style.display = "none";
       this.valueUsername = '';
-      this.status = true;
-    } else if (username.lenght < 4 && username.lenght > 20) {
-      this.valueUsername = 'Nome de usuário deve ter entre 4 e 20 caracteres'
-      document.getElementById('alert-username').style.display = "block";
+      this.statusUsername = true;
     } else {
       this.valueUsername = 'Digite apenas letras e números';
       document.getElementById('alert-username').style.display = "block";
+      this.statusUsername = false;
+    } if (username.length < 4 || username.length > 20) {
+      this.valueUsername = 'Nome de usuário deve ter entre 4 e 20 caracteres'
+      document.getElementById('alert-username').style.display = "block";
+      this.statusUsername = false;
+    } 
+  }
+
+  onKeyEmail(e: any) {
+    var email = e.target.value;
+    if(this.isEmailValid(email)){
+      document.getElementById('alert-email').style.display = "none";
+      this.valueEmail = '';
+      this.statusEmail = true;
+    } else {
+      document.getElementById('alert-email').style.display = "block";
+      this.valueEmail = 'Formato do E-mail está incorreto';
+      this.statusEmail = false;
+    } if(email.length < 4) {
+      document.getElementById('alert-email').style.display = "block";
+      this.valueEmail = 'Formato do E-mail está incorreto';
+      this.statusEmail = false;
     }
   }
 
-  onKeyConfirmPassword(e: any) {
-    var confirmPassword = e.target.value;
-
-    if(this.validator(confirmPassword, this.password)){
+  onKeyValidatorPassword(e: any) {
+    if(this.validator(this.confirmPassword, this.password)){
       document.getElementById('alert-password').style.display = "none";
       this.valuePassword = '';
-      this.status = true;
+      this.statusPassword = true;
     }
     else {
       this.valuePassword = 'A confirmação de senha não corresponde';
       document.getElementById('alert-password').style.display = "block";
-      this.status = false;
+      this.statusPassword = false;
     }
   }
 
@@ -96,6 +121,17 @@ export class RegisterFormComponent implements OnInit {
     }
     else{
       return true;
+    }
+  }
+
+  isEmailValid(email) {
+    var format1 = /[ @]/;
+    var format2 = /[ .]/;
+    if(format1.test(email) && format2.test(email)) {
+      return true;
+    }
+    else {
+      return false;
     }
   }
 
@@ -110,7 +146,7 @@ export class RegisterFormComponent implements OnInit {
   }
 
   clickFirstButton() {
-       if(this.status){
+       if(this.statusPassword && this.statusUsername && this.statusEmail){
             document.getElementById("firstPart").style.display = "none";
             document.getElementById("secondPart").style.display = "block";
             document.querySelector('#registerBtn').removeAttribute('disabled');
