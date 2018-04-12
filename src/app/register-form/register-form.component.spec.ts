5c0ee401581c46bf9fad8097907ee5e2163553df
filipetoyoshima <filipetoyoshima@gmail.com>
2018-04-11 22:35:43 -0300

    Added register forms unit tests

--- src/app/register-form/register-form.component.spec.ts
+++ src/app/register-form/register-form.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router'
 import { RequestsService } from '../requests.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -31,4 +32,34 @@ describe('RegisterFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should check username lenght', () => {
+    expect(component.isUsernameValid('top')).toBeFalsy;
+    expect(component.isUsernameValid('')).toBeFalsy;
+    expect(component.isUsernameValid('thisIsASuperLargeUsernameThatShouldNotBeAcceptd')).toBeFalsy;
+    expect(component.isUsernameValid('ValidUserName')).toBeTruthy;
+  })
+
+  it('should not accept username with special characters', () => {
+    expect(component.isUsernameValid('two words')).toBeFalsy;
+    expect(component.isUsernameValid('V1D@_LoK4')).toBeFalsy;
+    expect(component.isUsernameValid('User42')).toBeTruthy;
+  })
+
+  it('should check password lenght', () => {
+    expect(component.isValidPassword('123')).toBeFalsy;
+    expect(component.isValidPassword('')).toBeFalsy;
+    expect(component.isValidPassword('if your password is that big, another thing may not to be')).toBeFalsy;
+    expect(component.isValidPassword('MyGirlfriendBirthDate')).toBeTruthy;
+  })
+
+  it('should check if password is confirmed', () => {
+    expect(component.isConfirmedPassword('firstOne', 'secondOne')).toBeFalsy;
+    expect(component.isConfirmedPassword('twoEquals', 'twoEquals')).toBeTruthy;
+  })
+
+  it('should check email syntax', () => {
+    expect(component.isEmailValid('email.com.br')).toBeFalsy;
+    expect(component.isEmailValid('email@true.com')).toBeTruthy;
+  })
 });
