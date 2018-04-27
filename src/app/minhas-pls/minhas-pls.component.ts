a10df1970fae5421b7ee89c88e458057556c4972
Lucas362 <lucasvitorifg@gmail.com>
2018-04-27 13:19:49 -0300

    Fixed votes page style and pagination and changed the votes proposition get
    Co-authored-by: Rossicler <hellslimm@hotmail.com>

--- src/app/minhas-pls/minhas-pls.component.ts
+++ src/app/minhas-pls/minhas-pls.component.ts
@@ -11,33 +11,35 @@ export class MinhasPlsComponent implements OnInit {
 
   numberPLsVoted: number;
   pages: Array<number> = [1];
+  itemsPerPage = 10;
 
   proposition: Array<PropositionModel> = [
-  {
-    proposition_id: 0,
-    proposition_type: '',
-    proposition_type_initials: '',
-    number: 0,
-    year: 0,
-    abstract: '',
-    processing: '',
-    situation: '',
-    url_full: ''
-  }
-];
+    {
+      proposition_id: 0,
+      proposition_type: '',
+      proposition_type_initials: '',
+      number: 0,
+      year: 0,
+      abstract: '',
+      processing: '',
+      situation: '',
+      url_full: ''
+    }
+  ];
 
   constructor(private requester: RequestsService) { }
 
   ngOnInit() {
     this.numberPLsVoted = 1;
-    // this.propositions(10);
+    //this.propositions(10);
   }
 
   propositions(offset: number) {
-    this.requester.getVotedProposition(offset).subscribe( response => {
+    this.pages = [1];
+    this.requester.getVotedProposition(offset * this.itemsPerPage).subscribe( response => {
       this.proposition = response['results'];
       this.numberPLsVoted = response['count'];
-      for (let i = 2; i <= this.numberPLsVoted / 10; i++) {
+      for (let i = 2; i <= this.numberPLsVoted / this.itemsPerPage; i++) {
         this.pages.push(i);
       }
       console.log(this.proposition);
