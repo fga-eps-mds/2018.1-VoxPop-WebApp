355346ecff69d39a2b5a251ceda649893aad63e1
Lucas362 <lucasvitorifg@gmail.com>
2018-04-27 09:51:33 -0300

    fixed the get proposition and ngOnInit
    Co-authored-by: Rossicler Jr <hellslimm@hotmail.com>

--- src/app/minhas-pls/minhas-pls.component.ts
+++ src/app/minhas-pls/minhas-pls.component.ts
@@ -11,17 +11,35 @@ export class MinhasPlsComponent implements OnInit {
   numberPLsVoted: number;
   pages: Array<number> = [1];
 
-  constructor(private requester: RequestsService,) { }
+  proposition: any = {
+    proposition_id: 0,
+    proposition_type: '',
+    proposition_type_initials: '',
+    number: 0,
+    year: 0,
+    abstract: '',
+    processing: '',
+    situation: '',
+    url_full: ''
+  };
+
+  constructor(private requester: RequestsService) { }
 
   ngOnInit() {
-    this.numberPLsVoted = 0;
-    for (let i = 2; i <= this.numberPLsVoted / 10; i++) {
-      this.pages.push(i);
-    }
+    this.numberPLsVoted = 100;
+    this.propositions(10);
   }
 
-  proposition(offset: number) {
-    this.requester.getVotedProposition(offset);
+  propositions(offset: number) {
+    this.requester.getVotedProposition(offset).subscribe( response => {
+      this.proposition = response;
+      this.numberPLsVoted = this.proposition.count;
+      for (let i = 2; i <= this.numberPLsVoted / 10; i++) {
+        this.pages.push(i);
+      }
+      console.log(this.proposition);
+      console.log(this.numberPLsVoted);
+    });
   }
 
 }
