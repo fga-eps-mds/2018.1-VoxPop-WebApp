0f30107661fe39bcd503ed9c5a0fb968615fe2b9
Eduardo Lima <eduardolimrib@gmail.com>
2018-05-02 13:44:22 -0300

    Added edit-page component.
    Co-authored-by: rossicler <hellslimm@hotmail.com>

---
+++ src/app/edit-page/edit-page.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { RegisterFormComponent } from '../register-form/register-form.component';
+import { Router } from '@angular/router';
+import { RequestsService } from '../requests.service';
+import { CookieService } from 'ngx-cookie-service';
+import { TokenService } from '../token.service';
+
+@Component({
+  selector: 'app-edit-page',
+  templateUrl: './edit-page.component.html',
+  styleUrls: ['./edit-page.component.css']
+})
+
+export class EditPageComponent implements OnInit {
+
+  valuePassword = '';
+  valueInvalidPassword = '';
+  valueUsername = '';
+  valueEmail = '';
+  valueInvalidInput = '';
+  password = '';
+  confirmPassword = '';
+  username = '';
+  statusPassword = false;
+  statusValidPassword = false;
+  statusUsername = false;
+  statusEmail = false;
+  danger = '#d9534f';
+  sucess = '#5cb85c';
+
+  tokenValue = '';
+  idValue = 0;
+  user: any;
+
+  constructor(private router:Router,
+              private requester:RequestsService,
+              private cookieService:CookieService,
+              private token:TokenService) { }
+
+  ngOnInit() {
+    let user: any;
+    this.tokenValue = this.cookieService.get('token');
+    this.token.checkToken(this.tokenValue);
+    this.idValue = +this.cookieService.get('userID');
+    console.log(this.idValue);
+    this.requester.getUser(this.idValue).subscribe( response =>{
+      this.user = response['body'];
+      console.log(this.user);
+    });
+  }
+
+}
