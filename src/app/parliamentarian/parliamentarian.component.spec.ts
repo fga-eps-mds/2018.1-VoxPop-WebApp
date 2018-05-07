974cec86392d645f0a718a4573ad9c6c9a6325d0
filipetoyoshima <filipetoyoshima@gmail.com>
2018-05-07 15:54:57 -0300

    Fixed file mistake, wrote in .spec.ts instead .ts
    Co-authored-by: Lucas <lucasvitorifg@gmail.com>

--- src/app/parliamentarian/parliamentarian.component.spec.ts
+++ src/app/parliamentarian/parliamentarian.component.spec.ts
@@ -1,55 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { RequestsService } from '../requests.service';
-import { PropositionModel } from '../../models/proposition';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-@Component({
-  selector: 'app-see-pl',
-  templateUrl: './see-pl.component.html',
-  styleUrls: ['./see-pl.component.css']
-})
-export class SeePlComponent implements OnInit {
+import { ParliamentarianComponent } from './parliamentarian.component';
 
-  numberPLs: number;
-  pages: Array<number> = [1];
-  itemsPerPage = 50;
-  offset = 1;
+describe('ParliamentarianComponent', () => {
+  let component: ParliamentarianComponent;
+  let fixture: ComponentFixture<ParliamentarianComponent>;
 
-  parliamentarians: any = [
-    {
-      parliamentarian_id: null,
-      name: '',
-      gender: '',
-      federal_unit: '',
-      photo: '',
-    }
-  ];
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ParliamentarianComponent ]
+    })
+    .compileComponents();
+  }));
 
-  constructor(
-    private requester: RequestsService,
-  ) { }
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ParliamentarianComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-
-  ngOnInit() {
-    this.propositions(1);
-  }
-
-  propositions(offset: number) {
-    let req: any;
-    this.parliamentarians = [];
-
-    req =  this.requester.getParliamentarian();
-    this.handleParliamentariansResponse(req, this.offset);
-    return req;
-  }
-
-  handleParliamentariansResponse(request, offset) {
-    this.requester.getParliamentarian().subscribe( response => {
-      this.parliamentarians = response['results'];
-      this.numberPLs = response['count'];
-      console.log(this.parliamentarians);
-      console.log(this.numberPLs);
-    });
-  }
-
-}
-
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/parliamentarian/parliamentarian.component.ts
+++ src/app/parliamentarian/parliamentarian.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestsService } from '../requests.service';
+import { PropositionModel } from '../../models/proposition';
 
 @Component({
   selector: 'app-parliamentarian',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParliamentarianComponent implements OnInit {
 
-  constructor() { }
+  numberPLs: number;
+  pages: Array<number> = [1];
+  itemsPerPage = 50;
+  offset = 1;
+
+  parliamentarians: any = [
+    {
+      parliamentarian_id: null,
+      name: '',
+      gender: '',
+      federal_unit: '',
+      photo: '',
+    }
+  ];
+
+  constructor(
+    private requester: RequestsService,
+  ) { }
+
 
   ngOnInit() {
+    this.getParliamentarians(1);
+  }
+
+  getParliamentarians(offset: number) {
+    let req: any;
+    this.parliamentarians = [];
+
+    req =  this.requester.getParliamentarian();
+    this.handleParliamentariansResponse(req, this.offset);
+    return req;
+  }
+
+  handleParliamentariansResponse(request, offset) {
+    this.requester.getParliamentarian().subscribe( response => {
+      this.parliamentarians = response['results'];
+      this.numberPLs = response['count'];
+      console.log(this.parliamentarians);
+      console.log(this.numberPLs);
+    });
   }
 
 }
+
