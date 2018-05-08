0be042ee2cf5ee7b1e5c23b5dd788fb894c02d21
ericGOD24 <ericobandeira@hotmail.com>
2018-05-08 15:12:37 -0300

    Added 'see politician' component.
    Co-authored-by: Eduardo Lima <eduardolimrib@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MinhasPlsComponent } from './minhas-pls/minhas-pls.component';
 import { PropositionsComponent } from './propositions/propositions.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SeePlComponent } from './see-pl/see-pl.component';
+import { SeePoliticianComponent } from './see-politician/see-politician.component';
 
 const appRoutes: Routes = [
   {
@@ -65,6 +66,7 @@ const appRoutes: Routes = [
     PropositionsComponent,
     SidebarComponent,
     SeePlComponent,
+    SeePoliticianComponent,
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
---
+++ src/app/see-politician/see-politician.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-see-politician',
+  templateUrl: './see-politician.component.html',
+  styleUrls: ['./see-politician.component.css']
+})
+export class SeePoliticianComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
