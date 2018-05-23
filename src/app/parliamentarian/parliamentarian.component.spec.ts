e3d1cd76542549db2d89b0f4c25f4dc8974c5233
Rossicler Rodrigues Pires Júnior <hellslimm@hotmail.com>
2018-05-23 15:52:42 -0300

    Added RouterTestingModule paths on parliamentarian and user-following
    Co-authored-by: Erico <ericobandeira@hotmail.com>

--- src/app/parliamentarian/parliamentarian.component.spec.ts
+++ src/app/parliamentarian/parliamentarian.component.spec.ts
@@ -2,6 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ParliamentarianComponent } from './parliamentarian.component';
 import { RouterModule } from '@angular/router';
+import { RequestsService } from '../requests.service';
+import { TokenService } from '../token.service';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ParliamentarianComponent', () => {
   let component: ParliamentarianComponent;
@@ -10,9 +15,15 @@ describe('ParliamentarianComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterModule,
+        RouterTestingModule.withRoutes([{ path: 'parliamentarians/:id', component: ParliamentarianComponent}]),
+        HttpClientModule,
       ],
-      declarations: [ ParliamentarianComponent ]
+      declarations: [ ParliamentarianComponent ],
+      providers: [
+        RequestsService,
+        TokenService,
+        CookieService,
+      ]
     })
     .compileComponents();
   }));
--- src/app/user-following/user-following.component.spec.ts
+++ src/app/user-following/user-following.component.spec.ts
@@ -2,6 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserFollowingComponent } from './user-following.component';
 import { RouterModule } from '@angular/router';
+import { RequestsService } from '../requests.service';
+import { TokenService } from '../token.service';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('UserFollowingComponent', () => {
   let component: UserFollowingComponent;
@@ -10,9 +15,15 @@ describe('UserFollowingComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterModule,
+        RouterTestingModule.withRoutes([{ path: 'parliamentarians/:id', component: UserFollowingComponent}]),
+        HttpClientModule,
       ],
-      declarations: [ UserFollowingComponent ]
+      declarations: [ UserFollowingComponent ],
+      providers: [
+        RequestsService,
+        TokenService,
+        CookieService,
+      ]
     })
     .compileComponents();
   }));
