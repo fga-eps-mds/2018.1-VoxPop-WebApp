7eed5fcb630aebde0289d24b66cc91df7be6fba7
Lucas362 <lucasvitorifg@gmail.com>
2018-05-22 21:55:00 -0300

    Updated register form component to support automatic bind
    Co-authored-by: Eduardo <eduardolimrib@gmail.com>

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -12,37 +12,35 @@ import { InputValidatorService } from '../input-validator.service';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
-
+  user: any = {
+    username: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    social_information: {
+      federal_unit: null,
+      city: null,
+      income: null,
+      education: null,
+      job: '',
+      birth_date: ''
+    }
+  };
   constructor(private router: Router,
-              private requester: RequestsService,
-              private validator: InputValidatorService,
+    private requester: RequestsService,
+    private validator: InputValidatorService,
   ) { }
 
   ngOnInit() {
   }
 
-  registerUser(e) {
-    e.preventDefault();
-    let user: UserModel;
 
-    user = {
-      username: e.target.elements[0].value,
-      first_name: e.target.elements[1].value,
-      last_name: e.target.elements[2].value,
-      password: e.target.elements[3].value,
-      email: e.target.elements[5].value,
-      social_information: {
-        federal_unit: e.target.elements[7].value,
-        city: e.target.elements[8].value,
-        income: e.target.elements[9].value,
-        education: e.target.elements[10].value,
-        job: e.target.elements[11].value,
-        birth_date: e.target.elements[12].value
-      }
-     };
+  registerUser() {
     // TODO - adicionar validação de criação. Checar http status code = 201.
     // AINDA é TODO /\
-    this.requester.postUser(user).subscribe(response => {
+    console.log(this.user);
+    this.requester.postUser(this.user).subscribe(response => {
       const statusUser = response.status;
       console.log('STATUS CODE RETURNED ON USER: ' + statusUser);
 
