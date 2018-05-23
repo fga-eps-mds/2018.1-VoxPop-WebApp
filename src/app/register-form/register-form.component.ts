234eefddfcbe688c6efa0477cfa5d630543fd8a5
Lucas362 <lucasvitorifg@gmail.com>
2018-05-23 14:47:15 -0300

    Added error handler and alert to register form
    Co-authored-by: Toyoshima <filipetoyoshima@gmail.com>

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -27,6 +27,7 @@ export class RegisterFormComponent implements OnInit {
       birth_date: ''
     }
   };
+
   constructor(private router: Router,
     private requester: RequestsService,
     private validator: InputValidatorService,
@@ -39,22 +40,34 @@ export class RegisterFormComponent implements OnInit {
   registerUser() {
     // TODO - adicionar validação de criação. Checar http status code = 201.
     // AINDA é TODO /\
-    console.log(this.user);
-    this.requester.postUser(this.user).subscribe(response => {
+    // console.log(this.user);
+    let req;
+    req = this.requester.postUser(this.user);
+    this.registerUserHandler(req);
+    return req;
+  }
+
+  registerUserHandler(request) {
+    request.subscribe(response => {
       const statusUser = response.status;
-      console.log('STATUS CODE RETURNED ON USER: ' + statusUser);
+      // console.log('STATUS CODE RETURNED ON USER: ' + statusUser);
 
         if (this.requester.didSucceed(statusUser)) {
           this.router.navigate(['']);
         }
+    },
+    error => {
+        console.log(error);
+        const statusAuth = error.status;
+        this.errorHandler(statusAuth);
     });
-
   }
 
- clickReturnButton() {
-      document.getElementById('firstPart').style.display = 'block';
-      document.getElementById('secondPart').style.display = 'none';
-      document.querySelector('#registerBtn').setAttribute('disabled', 'disabled');
+  errorHandler (status) {
+    if (status === 500) {
+        document.getElementById('alert-invalid').style.display = 'block';
+        return false;
+    }
 }
 
 }
