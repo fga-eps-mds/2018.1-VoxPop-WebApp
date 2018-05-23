11576dff1d6d02932ef6a755746e7acbce08946e
Rossicler Rodrigues Pires Júnior <hellslimm@hotmail.com>
2018-05-23 15:52:00 -0300

    Added a fake response
    Co-authored-by: Erico <ericobandeira@hotmail.com>

--- src/app/main-page/main-page.component.spec.ts
+++ src/app/main-page/main-page.component.spec.ts
@@ -1,27 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, inject } from '@angular/core/testing';
 
 import { MainPageComponent } from './main-page.component';
 import { CookieService } from 'ngx-cookie-service';
 import { TokenService } from '../token.service';
 import { RequestsService } from '../requests.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MockBackend } from '@angular/http/testing';
+import { BaseRequestOptions, Http, ResponseOptions, Jsonp } from '@angular/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('MainPageComponent', () => {
   let component: MainPageComponent;
+  let requests: MockBackend;
   let fixture: ComponentFixture<MainPageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,
+        HttpClientTestingModule,
       ],
       providers: [
         RequestsService,
         CookieService,
-        TokenService
+        TokenService,
+        MockBackend,
       ],
       declarations: [ MainPageComponent ]
     })
+
     .compileComponents();
   }));
 
@@ -41,4 +48,25 @@ describe('MainPageComponent', () => {
 
     expect(component).toBeTruthy();
   });
+
+  it('should return a preposition', fakeAsync (() => {
+    const response = {
+      "results": [
+        {
+          "proposition_id": 12,
+          "proposition_type": 'Teste',
+          "proposition_type_initials": 'T',
+          "number": 32,
+          "year": 2009,
+          "abstract": 'Testando o response de uma preposition',
+          "processing": 'Teste',
+          "situation": 'Em espera',
+          "url_full": 'Teste'
+        }]
+    };
+
+    component.handlePropositionsResponse(response, 1, 0);
+    
+  }));
+
 });
