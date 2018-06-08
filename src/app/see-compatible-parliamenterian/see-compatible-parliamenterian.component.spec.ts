b9631c4930191bcc34a419d6a943b043ea2a085b
rossicler <hellslimm@hotmail.com>
2018-06-08 20:09:42 -0300

    Added tests on see-compatible-parliamenterian component
    Co-authored-by: Lucas <lucasvitorifg@gmail.com>

--- src/app/see-compatible-parliamenterian/see-compatible-parliamenterian.component.spec.ts
+++ src/app/see-compatible-parliamenterian/see-compatible-parliamenterian.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SeeCompatibleParliamenterianComponent } from './see-compatible-parliamenterian.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TokenService } from '../token.service';
+import { RequestsService } from '../requests.service';
+import { CookieService } from 'ngx-cookie-service';
 
 describe('SeeCompatibleParliamenterianComponent', () => {
   let component: SeeCompatibleParliamenterianComponent;
@@ -8,7 +14,17 @@ describe('SeeCompatibleParliamenterianComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SeeCompatibleParliamenterianComponent ]
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'parliamentarians/:id', component: SeeCompatibleParliamenterianComponent}]),
+        HttpClientModule,
+        HttpClientTestingModule,
+      ],
+      declarations: [ SeeCompatibleParliamenterianComponent ],
+      providers: [
+        CookieService,
+        TokenService,
+        RequestsService
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +38,11 @@ describe('SeeCompatibleParliamenterianComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the page', () => {
+    component.tokenValue = 'teste';
+    component.ngOnInit();
+    expect(component).toBeTruthy();
+  });
+
 });
