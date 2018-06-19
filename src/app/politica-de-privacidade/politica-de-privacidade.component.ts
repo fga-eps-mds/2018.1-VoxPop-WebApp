19f973464908325bbf79e22b0c83a981b0b0f269
Lucas362 <lucasvitorifg@gmail.com>
2018-06-18 22:41:59 -0300

    Added check token to contact us and politica de privacidade page
    Co-atuhored-by: Erico <ericobandeira@hotmail.com>

--- src/app/politica-de-privacidade/politica-de-privacidade.component.ts
+++ src/app/politica-de-privacidade/politica-de-privacidade.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestsService } from '../requests.service';
+import { CookieService } from 'ngx-cookie-service';
+import { TokenService } from '../token.service';
 
 @Component({
   selector: 'app-politica-de-privacidade',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PoliticaDePrivacidadeComponent implements OnInit {
 
-  constructor() { }
+  tokenValue = '';
+
+  constructor(private requester: RequestsService,
+              private cookieService: CookieService,
+              private token: TokenService) { }
 
   ngOnInit() {
+    this.tokenValue = this.token.getToken();
+    this.token.checkToken(this.tokenValue);
   }
 
 }
