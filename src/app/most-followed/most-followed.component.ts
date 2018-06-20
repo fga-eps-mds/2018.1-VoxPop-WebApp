718e40076764d786753b0f35ba8844ad26c02745
Rossicler Rodrigues Pires Júnior <hellslimm@hotmail.com>
2018-06-19 21:00:58 -0300

    Added new component and created a path to most-followed page
    Co-authored-by: Toyoshima <filipetoyoshima@gmail.com>

---
+++ src/app/most-followed/most-followed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-most-followed',
+  templateUrl: './most-followed.component.html',
+  styleUrls: ['./most-followed.component.css']
+})
+export class MostFollowedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
